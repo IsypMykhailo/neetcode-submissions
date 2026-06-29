1a299cb3e5ac826fd92855e7dd3d1ad200177391
Mykhailo <78875641+IsypMykhailo@users.noreply.github.com>
2026-06-28 19:35:50 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,50 @@
+class TimeMap {
+    class Pair {
+        String value;
+        int timestamp;
+
+        Pair(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+    Map<String, List<Pair>> map;
+
+    public TimeMap() {
+        map = new HashMap();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        } 
+        map.get(key).add(new Pair(value, timestamp));
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) {
+            return "";
+        }
+
+        List<Pair> list = map.get(key);
+        int left = 0;
+        int right = list.size() - 1;
+        String result = "";
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midTimestamp = list.get(mid).timestamp;
+
+            if (midTimestamp == timestamp) {
+                return list.get(mid).value;
+            } else if (midTimestamp < timestamp) {
+                result = list.get(mid).value;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
