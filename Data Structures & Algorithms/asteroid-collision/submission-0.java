61679860305095143093a2a548f006197d3de341
Mykhailo <78875641+IsypMykhailo@users.noreply.github.com>
2026-07-20 19:09:13 -0700

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            boolean alive = true;
+
+            while (alive && asteroid < 0 && !stack.isEmpty() && stack.peek() > 0) {
+                int top = stack.peek();
+
+                if (top < -asteroid) {
+                    stack.pop();
+                } else if (top == -asteroid) {
+                    stack.pop();
+                    alive = false;
+                } else {
+                    alive = false;
+                }
+            }
+
+            if (alive) {
+                stack.push(asteroid);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
